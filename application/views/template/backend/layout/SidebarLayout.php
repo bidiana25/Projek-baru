17cc68d71dcf0c23f6c31568b51b2d8fdfde6f0c
tantangin <irsan00mansyur@gmail.com>
2020-07-17 06:48:39 -0700

    sidebar

---
+++ application/views/template/backend/layout/SidebarLayout.php
@@ -0,0 +1,28 @@
+<div class="pcoded-navigation-label">Navigation</div>
+<ul class="pcoded-item pcoded-left-item">
+    <li class="pcoded-hasmenu">
+        <a href="javascript:void(0)" class="waves-effect waves-dark">
+            <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+            <span class="pcoded-mtext">Dashboard</span>
+        </a>
+        <ul class="pcoded-submenu">
+            <li class="">
+                <a href="<?= base_url("page/home"); ?>" class="submenu waves-effect waves-dark">
+                    <span class="pcoded-mtext">Default</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="<?= base_url("c_akun"); ?>" class="waves-effect waves-dark submenu">
+                    <span class="pcoded-mtext">Daftar Akun</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="dashboard-analytics.html" class="waves-effect waves-dark submenu">
+                    <span class="pcoded-mtext">Analytics</span>
+                    <span class="pcoded-badge label label-info ">NEW</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+
+</ul>
